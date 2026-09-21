3471a559ce4dd612b02b088fa2385cbe638ec28a
s3tupw1zard <50152835+s3tupw1zard@users.noreply.github.com>
2026-09-21 20:13:12 +0200

    Fix startup env assignment handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,7 @@ PARSED_STARTUP="$(printf '%s' "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')"
 
 printf '\033[1m\033[33mcontainer~ \033[0m%s\n' "${PARSED_STARTUP}"
 
-# Pelican startup commands intentionally support environment variable expansion.
-# shellcheck disable=SC2086
-eval "exec ${PARSED_STARTUP}"
+# Pelican startup commands can begin with environment assignments such as
+# PORT=25565. Prefixing the parsed command with `env` lets those assignments
+# be applied before replacing this shell with the actual process.
+eval "exec env ${PARSED_STARTUP}"
